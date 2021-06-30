615599cd909f9ababec65e7d287755617b98fbdb
gailazar300 <gail@jfrog.com>
2021-06-30 15:22:10 +0300

    reset changes

--- exec.go
+++ exec.go
@@ -2,8 +2,9 @@ package gocmd
 
 import (
 	"github.com/jfrog/gocmd/executers"
+	"github.com/jfrog/gocmd/params"
 )
 
-func RunWithFallback(goArg []string) error {
-	return executers.RunWithFallbacks(goArg)
+func RunWithFallback(goArg []string, resolverDeployer *params.ResolverDeployer) error {
+	return executers.RunWithFallback(goArg, resolverDeployer)
 }
